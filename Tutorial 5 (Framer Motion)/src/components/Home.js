00b8dfe4f7ad09a4978f118d3c20f35f83da824b
arminv <arminv@users.noreply.github.com>
2020-09-06 01:36:16 -0400

    End of #3

--- Tutorial 5 (Framer Motion)/src/components/Home.js
+++ Tutorial 5 (Framer Motion)/src/components/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+    <motion.div className='home container' animate={{}}>
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to='/base'>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
