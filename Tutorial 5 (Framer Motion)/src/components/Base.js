1f950325156e9191ca699b61b79090d63ea12b8e
arminv <arminv@users.noreply.github.com>
2020-09-06 01:48:42 -0400

    End of #5

--- Tutorial 5 (Framer Motion)/src/components/Base.js
+++ Tutorial 5 (Framer Motion)/src/components/Base.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
-    <div className="base container">
-
+    <motion.div
+      className='base container'
+      initial={{ x: '100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', delay: 0.5 }}
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
+        {bases.map((base) => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
             <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </li>
-          )
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
-          <Link to="/toppings">
+        <motion.div
+          className='next'
+          initial={{ x: '-100vw' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 120 }}
+        >
+          <Link to='/toppings'>
             <button>Next</button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
