14c788193222d31e09f408f225f4f0aaea20e096
arminv <arminv@users.noreply.github.com>
2020-09-08 00:02:39 -0400

    End of #8

--- Tutorial 5 (Framer Motion)/src/components/Base.js
+++ Tutorial 5 (Framer Motion)/src/components/Base.js
@@ -2,15 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5,
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw',
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 120,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div
       className='base container'
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -31,10 +59,10 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div
+          variants={nextVariants}
           className='next'
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
+          initial='hidden'
+          animate='visible'
         >
           <Link to='/toppings'>
             <motion.button
